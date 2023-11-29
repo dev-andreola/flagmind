be1ae310774551c4c00502d5eba62cf039078a00
vt-code <dev.andreola@gmail.com>
2023-11-29 10:00:55 -0300

    Adding score funcionality

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Quiz = () => {
   const quizData = [
@@ -9,22 +9,58 @@ const Quiz = () => {
       b: "Japan",
       c: "Brazil",
       d: "Bulgaria",
+      res: "c",
     },
     {
       country: "Colombia",
       flag: "https://www.worldometers.info/img/flags/co-flag.gif",
       a: "India",
-      b: "Japan",
+      b: "Colombia",
       c: "Madagascar",
       d: "Haiti",
+      res: "b",
+    },
+    {
+      country: "Colombia",
+      flag: "https://www.worldometers.info/img/flags/co-flag.gif",
+      a: "India",
+      b: "Colombia",
+      c: "Madagascar",
+      d: "Haiti",
+      res: "b",
     },
   ];
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
+  const [score, setScore] = useState(0);
+
+  const [userAnswer, setUserAnswer] = useState(undefined);
+
+  const handleAnswerChange = (event) => {
+    setUserAnswer(event.target.id);
+  };
+
+  // BUTTON ONCLICK EVENT
   const submit = () => {
-    setCurrentQuestion((current) => current + 1);
-    
+    // IF DON'T HAVE ANY CHECKED
+    if (!userAnswer) {
+      alert("Você precisa preencher uma das opções!");
+      return;
+    }
+    // GET THE ANSWER RIGHT
+    if (userAnswer === quizData[currentQuestion].res) {
+      setScore((prevScore) => prevScore + 1);
+      alert("Acertou! Seu score atual é: " + (score + 1));
+    }
+    // IF THE QUESTIONS END
+    if (currentQuestion >= quizData.length - 1) {
+      alert("Você terminou! Seu score final é: " + score);
+    } else {
+      setCurrentQuestion((current) => current + 1);
+    }
+    // RESET USER ANSWER
+    setUserAnswer(undefined);
   };
 
   return (
@@ -43,61 +79,31 @@ const Quiz = () => {
       {/* QUIZ ANSWERS CONTAINER */}
       <div className="bg-slate-100 rounded-t-lg p-3 w-2/3">
         <ul className="grid grid-cols-2" id="questionList">
-          <label className="cursor-pointer" htmlFor="a">
-            <li className="p-1 m-1 flex items-center justify-start gap-x-1 bg-slate-200 rounded-lg cursor-pointer">
-              <input
-                type="radio"
-                name="answer"
-                id="a"
-                className="cursor-pointer"
-              />
-
-              {quizData[currentQuestion].a}
-            </li>
-          </label>
-          <label className="cursor-pointer" htmlFor="b">
-            <li className="p-1 m-1 flex items-center justify-start gap-x-1 bg-slate-200 rounded-lg cursor-pointer">
-              <input
-                type="radio"
-                name="answer"
-                id="b"
-                className="cursor-pointer"
-              />
-
-              {quizData[currentQuestion].b}
-            </li>
-          </label>
-          <label className="cursor-pointer" htmlFor="c">
-            <li className="p-1 m-1 flex items-center justify-start gap-x-1 bg-slate-200 rounded-lg cursor-pointer">
-              <input
-                type="radio"
-                name="answer"
-                id="c"
-                className="cursor-pointer"
-              />
-
-              {quizData[currentQuestion].c}
-            </li>
-          </label>
-          <label className="cursor-pointer" htmlFor="d">
-            <li className="p-1 m-1 flex items-center justify-start gap-x-1 bg-slate-200 rounded-lg cursor-pointer">
-              <input
-                type="radio"
-                name="answer"
-                id="d"
-                className="cursor-pointer"
-              />
-
-              {quizData[currentQuestion].d}
-            </li>
-          </label>
+          {["a", "b", "c", "d"].map((answerId) => (
+            <label htmlFor={answerId}>
+              <li
+                key={answerId}
+                className="p-1 m-1 flex items-center justify-start gap-x-1 bg-slate-200 rounded-lg cursor-pointer"
+              >
+                <input
+                  type="radio"
+                  name="answer"
+                  id={answerId}
+                  className="cursor-pointer"
+                  onChange={handleAnswerChange}
+                  checked={userAnswer === answerId}
+                />
+                {quizData[currentQuestion][answerId]}
+              </li>
+            </label>
+          ))}
         </ul>
       </div>
       {/* QUIZ SUBMIT BUTTON */}
       <button
         type="button"
         onClick={submit}
-        className="py-3 text-white font-bold tracking-widest uppercase w-full rounded-b-lg bg-slate-600 hover:bg-green-800 text-center"
+        className="py-3 text-white font-bold tracking-widest uppercase w-full rounded-b-lg bg-slate-500 hover:bg-green-800 text-center"
       >
         Enviar
       </button>
